98ef0823e3f59f8941725cd55ee1f36cd1903cf4
vvisionnn <wz546500392@gmail.com>
2020-12-12 10:24:23 +0800

    add build track url function

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"github.com/jackpal/bencode-go"
 	"io"
+	"net"
+	"net/url"
 	"os"
 	"reflect"
+	"strconv"
 )
+
+type TorrentFile struct {
+	// announce server
+	Announce	string
+	// torrent file hash code, SHA-1
+	InfoHash	[20]byte
+	PieceHashes	[][20]byte
+	PieceLength	int
+	Length		int
+	Name		string
+}
 
 type bencodeInfo struct {
 	Pieces      string `bencode:"pieces"`
@@ -22,6 +37,30 @@ type bencodeTorrent struct {
 	Info         bencodeInfo `bencode:"info"`
 }
 
+type Peer struct {
+	IP		net.IP
+	Port	uint16
+}
+
+
+// Unmarshal parse peer IP from buffer
+func Unmarshal(peersBin []byte) ([]Peer, error) {
+	const peerSize = 6	// 4 for IP, 2 for port
+	numPeers := len(peersBin) / peerSize
+	if len(peersBin) % peerSize != 0 {
+		err := fmt.Errorf("received malformed peers")
+		return nil, err
+	}
+
+	peers := make([]Peer, numPeers)
+	for i := 0; i < numPeers; i++ {
+		offset := i * peerSize
+		peers[i].IP = peersBin[offset: offset+4]
+		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4: offset+6])
+	}
+	return peers, nil
+}
+
 // Open parses a torrent file
 func Open(r io.Reader) (*bencodeTorrent, error) {
 	bto := bencodeTorrent{}
@@ -32,16 +71,32 @@ func Open(r io.Reader) (*bencodeTorrent, error) {
 	return &bto, nil
 }
 
-func main() {
-	filePath := ""
-	f, _ := os.OpenFile(
-		filePath,
-		os.O_RDONLY,
-		0755)
-	btoP, err := Open(f)
-	if err != nil {
-		fmt.Println(err)
+
+func (t *TorrentFile) buildTrackerURL(peerID [20]byte, port int) (string, error) {
+	base, err := url.Parse(t.Announce)
+	if err != nil { return "", err }
+
+	params := url.Values{
+		// info hash identify the torrent file we're trying to download
+		"info_hash":	[]string{ string(t.InfoHash[:]) },
+		// peer_id identify ourself to tracker server, random bytes
+		"peer_id":		[]string{ string(peerID[:]) },
+		"port":			[]string{ strconv.Itoa(port) },
+		"uploaded":		[]string{ "0" },
+		"downloaded":	[]string{ "0" },
+		"compact":		[]string{ "1" },
+		"left": 		[]string{ strconv.Itoa(t.Length) },
 	}
+	base.RawQuery = params.Encode()
+	return base.String(), nil
+}
+
+
+func main() {
+	filePath := "test.torrent"
+	f, _ := os.OpenFile(filePath, os.O_RDONLY, 0755)
+	btoP, err := Open(f)
+	if err != nil { fmt.Println(err) }
 
 	t := reflect.TypeOf(*btoP)
 	for i := 0; i < t.NumField(); i++ {
